d8a4db9212bb7b5d006acdea54949277c479cf89
shl0mo <salomao.dacostacruz@gmail.com>
2023-08-14 19:26:15 -0400

    init

---
+++ src/script.js
@@ -0,0 +1,15 @@
+function validar_email (email) {
+	// escreva seu código aqui
+}
+
+function validar_cep (cep) {
+	// escreva seu código aqui
+}
+
+function validar_cpf (cpf) {
+	// escreva seu código aqui
+}
+
+function validar_telefone (telefone) {
+	// escreva seu código aqui
+}
