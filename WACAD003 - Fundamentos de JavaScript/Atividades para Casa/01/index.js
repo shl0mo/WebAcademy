0982dc202c7e3dbe7002ea703904190837696561
shl0mo <salomao.dacostacruz@gmail.com>
2023-09-14 13:39:24 -0400

    Fundamentos de JavaScript - História

--- WACAD003 - Fundamentos de JavaScript/Atividades para Casa/01/index.js
+++ WACAD003 - Fundamentos de JavaScript/Atividades para Casa/01/index.js
@@ -1,1 +1,33 @@
+/* 1. COMPLETE VARIABLE AND FUNCTION DEINITIONS */
+const customName = document.getElementById('customname')
+const radomize = document.querySelector('.radomize')
+const story = document.querySelector('.story')
 
+function randomValueFromArray (array) {
+	const random = Math.floor(Math.random() * array.length)
+	return array[random]
+}
+
+/* 2. TEXT STRING VARIABLE */
+
+const random_story = `
+	Certo dia, JavaScript, uma linguagem pensada para o paradigma funcional, conheceu Java, uma linguagem feita para orientação a objetos. JavaScript disse a Java que se chamava assim porque seus pais eram grandes fãs deste e que por causa do nome que lhe foi dado, se inspirava muito em Java. Java então, questionou se JavaScript sabia sobre orientação a objetos, o qual respondeu que sim, o que fez com que Java imediatamente o convidadesse este para trabalhar em sua empresa, a Oracle, o que foi aceito por JavaScript. No primeiro dia de trabalho, no entanto, ao ver como JavaScript se atrapalhava nos conceitos de orientação a objetos, ao notar a roupa cafona que ele estava usando e como xingava o computador por tentar sem sucesso usar um objeto sem ter definido um modelo geral para ele, Java o demitiu por ver que aquela não era a sua função, já que JavaScript era uma linguagem sem classe
+`
+
+/* 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION */
+
+randomize.addEventListener('click', result)
+
+
+function result () {
+	if (customName.value !== '') {
+		const name = customName.value
+	}
+	if (document.getElementById('uk').checked) {
+		const weight = Math.round(300)
+		const temperature = Math.round(94)
+	}
+
+	story.textContext = ''
+	story.style.visibility = 'visible'
+}
