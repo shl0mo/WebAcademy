3c8e236984bfe5a89eca84d3367bf06f7805e120
shl0mo <salomao.dacostacruz@gmail.com>
2023-09-14 20:04:45 -0400

    Fundamentos de JavaScript - Atividades para Casa - 1 - formato geral

--- WACAD003 - Fundamentos de JavaScript/Atividades para Casa/01/index.js
+++ WACAD003 - Fundamentos de JavaScript/Atividades para Casa/01/index.js
@@ -1,5 +1,12 @@
 /* 1. COMPLETE VARIABLE AND FUNCTION DEINITIONS */
-const story = document.querySelector('.story')
+function fetchNames () {
+	fetch('https://random-word-api.herokuapp.com/all')
+	.then((res) => {
+		return res.json().then((data) => {
+			console.log(data)
+		})
+	})
+}
 
 function randomValueFromArray (array) {
 	const random = Math.floor(Math.random() * array.length)
@@ -8,9 +15,19 @@ function randomValueFromArray (array) {
 
 /* 2. TEXT STRING VARIABLE */
 
-const random_story = `
+let randomStory = `
 	Certo dia, JavaScript, uma linguagem pensada para o paradigma funcional, conheceu Java, uma linguagem feita para orientação a objetos. JavaScript disse a Java que se chamava assim porque seus pais eram grandes fãs deste e que por causa do nome que lhe foi dado, se inspirava muito em Java. Java então, questionou se JavaScript sabia sobre orientação a objetos, o qual respondeu que sim, o que fez com que Java imediatamente o convidadesse este para trabalhar em sua empresa, a Oracle. O convite foi aceito por JavaScript. No primeiro dia de trabalho, no entanto, ao ver como JavaScript se atrapalhava nos conceitos de orientação a objetos, ao notar a roupa cafona que ele estava usando e como xingava o computador por tentar sem sucesso usar um "objeto" sem ter definido um modelo geral para ele, Java o demitiu por ver que aquela não era a sua função, já que JavaScript era uma linguagem sem classe
 `
+
+/* 
+ * o paradigma funcional
+ * orientação a objetos
+ * pais
+ * Oracle
+ * computador
+ * "objeto"
+ * classe
+ * */
 
 /* 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION */
 
@@ -21,15 +38,20 @@ const interval = setInterval(function () {
 
 
 function result () {
-	const customName = document.getElementById('customname')
-	if (customName.value !== '') {
-		const name = customName.value
+	const customName1 = document.getElementById('customname-1')
+	const customName2 = document.getElementById('customname-2')
+	let story = document.querySelector('.story')
+	if (customName1.value !== '' || customName2.value !== '') {
+		const name1 = customName1.value
+		const name2 = customName2.value
+		randomStory = randomStory.replace('JavaScript', name1)
+		randomStory = randomStory.replace('Java', name2)
 	}
 	if (document.getElementById('uk').checked) {
-		const weight = Math.round(300)
-		const temperature = Math.round(94)
-	}
 
-	story.textContext = ''
+	}
+	fetchNames()
+	console.log(randomStory)
+	story.innerText = randomStory
 	story.style.visibility = 'visible'
 }
