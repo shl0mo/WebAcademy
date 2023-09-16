68c02a46785f040f33934c4bf72b1fd5a7f604c3
shl0mo <salomao.dacostacruz@gmail.com>
2023-09-16 02:49:13 -0400

    Fundamentos de JS - Lab 02 - loadImages()

--- WACAD003 - Fundamentos de JavaScript/Laboratório 02/main.js
+++ WACAD003 - Fundamentos de JavaScript/Laboratório 02/main.js
@@ -1,18 +1,19 @@
+function loadImages () {
+	thumbBar = document.querySelector('.thumb-bar')
+	const imagesDescriptions = ['', '', '', '', '']
+	const numberOfImages = imagesDescriptions.length
+	for (let i = 0; i < numberOfImages; i++) {
+		const newImage = document.createElement('img');
+		newImage.setAttribute('src', `./images/pic${i+1}.jpg`);
+		newImage.setAttribute('alt', imagesDescriptions[i]);
+		thumbBar.appendChild(newImage);
+	}
+
+}
+
+/*
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-/* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+const overlay = document.querySelector('.overlay');*/
