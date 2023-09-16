8d8a22814d56c34dffb41fe8238d0dcfe6002006
shl0mo <salomao.dacostacruz@gmail.com>
2023-09-16 03:14:01 -0400

    Fundamentos de JS - Laboratório 02

--- WACAD003 - Fundamentos de JavaScript/Laboratório 02/main.js
+++ WACAD003 - Fundamentos de JavaScript/Laboratório 02/main.js
@@ -1,19 +1,38 @@
 function loadImages () {
-	thumbBar = document.querySelector('.thumb-bar')
-	const imagesDescriptions = ['', '', '', '', '']
+	const thumbBar = document.querySelector('.thumb-bar')
+	const imagesDescriptions = [
+		'Closeup of a human eye',
+		'',
+		'Purple and white flowers',
+		'Anubis and his bros on an Egyptian mural',
+		''
+	]
 	const numberOfImages = imagesDescriptions.length
 	for (let i = 0; i < numberOfImages; i++) {
-		const newImage = document.createElement('img');
-		newImage.setAttribute('src', `./images/pic${i+1}.jpg`);
-		newImage.setAttribute('alt', imagesDescriptions[i]);
+		const newImage = document.createElement('img')
+		newImage.setAttribute('src', `./images/pic${i+1}.jpg`)
+		newImage.setAttribute('alt', imagesDescriptions[i])
+		newImage.addEventListener('click', displayImage)
 		thumbBar.appendChild(newImage);
 	}
-
+	addDarkenButtonListener()
 }
 
-/*
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+function displayImage (event) {
+	const currentDisplayedImage = document.querySelector('.displayed-img')
+	const newDisplayedImageSrc = event.target.src
+	currentDisplayedImage.setAttribute('src', newDisplayedImageSrc)
+}
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');*/
+function addDarkenButtonListener () {
+	document.querySelector('.dark').addEventListener('click', darken)
+}
+
+function darken () {
+	const overlay = document.querySelector('.overlay')
+	if (overlay.style.backgroundColor === '') {
+		overlay.style.backgroundColor = 'rgba(0,0,0,0.5)'
+	} else {
+		overlay.style.backgroundColor = ''
+	}
+}
