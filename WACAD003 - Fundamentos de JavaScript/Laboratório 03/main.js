069fb304501bcd74d64be706637a4c3e87b88c98
shl0mo <salomao.dacostacruz@gmail.com>
2023-09-16 22:10:05 -0400

    Fundamentos de JS - Lab 3

--- WACAD003 - Fundamentos de JavaScript/Laboratório 03/main.js
+++ WACAD003 - Fundamentos de JavaScript/Laboratório 03/main.js
@@ -19,7 +19,7 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-function Ball(x, y, velX, velY, color, size) {
+function Triangle(x, y, velX, velY, color, size) {
   this.x = x;
   this.y = y;
   this.velX = velX;
@@ -28,14 +28,24 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-Ball.prototype.draw = function () {
+Triangle.prototype.draw = function () {
+  const x1 = this.x;
+  const y1 = this.y - (Math.sqrt(3) / 2) * this.size;
+  const x2 = this.x - this.size / 2;
+  const y2 = this.y + (Math.sqrt(3) / 2) * (this.size/ 2);
+  const x3 = this.x + this.size / 2;
+  const y3 = this.y + (Math.sqrt(3) / 2) * (this.size / 2);
+
   ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.lineTo(x3, y3);
   ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.closePath();
   ctx.fill();
 };
 
-Ball.prototype.update = function () {
+Triangle.prototype.update = function () {
   if (this.x + this.size >= width) {
     this.velX = -this.velX;
   }
@@ -56,59 +66,48 @@ Ball.prototype.update = function () {
   this.y += this.velY;
 };
 
-  Ball.prototype.collisionDetect = function () {
-    for (let j = 0; j < balls.length; j++) {
-      if (!(this === balls[j])) {
-        const dx = this.x - balls[j].x;
-        const dy = this.y - balls[j].y;
+  Triangle.prototype.collisionDetect = function () {
+    for (let j = 0; j < triangles.length; j++) {
+      if (!(this === triangles[j])) {
+        const dx = this.x - triangles[j].x;
+        const dy = this.y - triangles[j].y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + balls[j].size) {
-          balls[j].color = this.color =
-            "rgb(" +
-            random(0, 255) +
-            "," +
-            random(0, 255) +
-            "," +
-            random(0, 255) +
-            ")";
+        if (distance < this.size + triangles[j].size) {
+          triangles[j].color = this.color =
+            `rgba(0, 162, 232, ${random(0.4, 1)})`;
         }
+
       }
     }
 };
 
-let balls = [];
+let triangles = [];
 
-while (balls.length < 25) {
-  let size = random(10, 20);
-  let ball = new Ball(
-    // ball position always drawn at least one ball width
+while (triangles.length < 25) {
+  let size = random(10, 30);
+  let triangle = new Triangle(
+    // triangle position always drawn at least one triangle width
     // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
-    "rgb(" +
-      random(0, 255) +
-      "," +
-      random(0, 255) +
-      "," +
-      random(0, 255) +
-      ")",
+    `rgba(0, 162, 232, ${random(0.4, 1)})`,
     size,
   );
 
-  balls.push(ball);
+  triangles.push(triangle);
 }
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
-  for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+  for (let i = 0; i < triangles.length; i++) {
+    triangles[i].draw();
+    triangles[i].update();
+    triangles[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
